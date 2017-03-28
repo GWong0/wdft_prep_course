13eb8107ffa2a45df1c908461aee431a4b02f79d
Gerry Wong <WongGCW@gmail.com>
2017-03-28 14:43:42 -0400

    Tuesday Afternoon, Hope it works

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -3,8 +3,12 @@ Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
 
-
-
+for (var Number = 1; Number <=100; Number += 1)
+    console.log(Number);
+ if (number / 3 == 0)
+    console.log("Fizz");
+  if (number / 5 == 0)
+    console.log("Buzz");
 /*
 Exercise #2
 Write a loop that makes seven calls to console.log to output the following triangle:
@@ -17,3 +21,6 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+for ( var Pie = "#"; Pie.length <= 7; Pie +="#")
+    console.log(Pie);
